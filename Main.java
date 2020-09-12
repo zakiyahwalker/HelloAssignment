1188e31601915f0e80d0e5a8a0487ad523e1955c
zakiyahwalker <37126731+zakiyahwalker@users.noreply.github.com>
2020-09-12 18:39:49 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = input.nextLine();
+        System.out.println("Hello, " + name);
+    }
+}
